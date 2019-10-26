a7671de13077bdedd2c24dcf4ca2650bc9ff1266
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-25 20:30:20 -0500

    componentes life cycle

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,13 +1,24 @@
-import { Product } from './product.model';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from "./product.model";
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  OnDestroy
+} from "@angular/core";
 
-@Component({ // Decorator
+@Component({
+  // Decorator
   // MetaData from Component
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.scss"]
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<string> = new EventEmitter();
 
@@ -19,13 +30,48 @@ export class ProductComponent implements OnInit {
     description: 'bla bla bla bla bla'
   };*/
 
-  constructor() { }
+  constructor() {
+    console.log("1. constructor");
+  }
+  /*
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('2. ngOnChanges');
+    console.log(changes);
+  }
 
+  Es la forma nativa de angular de detectar cambios,
+  pero No es recomendable usarlo junto con ngDoCheck()
+*/
   ngOnInit() {
+    // Call data services
+    console.log("3. ngOnInit");
+  }
+
+  ngDoCheck() {
+    // Se pueden detectar cambios, con una implementación forzada o manual
+    console.log("4. ngDoCheck");
+  }
+
+  ngOnDestroy() {
+    console.log("5. ngOnDestroy");
   }
 
   addCart() {
-    console.log('added');
+    console.log("added");
     this.productClicked.emit(this.product.id);
   }
 }
+
+/*
+Component life cycle
+
+constructor // Crear componente y ponerlo en la interfaz
+ngOnChanges // Detectar cambios en inputs (se ejecuta en cada cambio)
+ngOnInit // Cuando el componente ya está listo en la interfaz grafica (rest api calls)
+ngDoCheck // Cuando los componentes hijos son creados y puestos en la interfaz
+  ngAfterContentInit
+  ngAfterContentChecked
+  ngAfterViewInit
+  ngAfterViewChecked
+ngOnDestroy // Cuando el elemento es quitado desde la interfaz
+*/
