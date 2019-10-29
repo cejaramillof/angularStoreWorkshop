5e8f12a518621636104d70a222ac08920d07f307
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-10-29 14:01:25 -0500

    admin module generated with --routing, and schematic created with ng g @angular/material:address-form componentName

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+
+
+@NgModule({
+  declarations: [ProductFormComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule
+  ]
+})
+export class AdminModule { }
