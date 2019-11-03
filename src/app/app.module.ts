15ec27557a8c19eec3725c95787ce03d7504a4a1
Carlos Jaramillo <cejarami@bancolombia.com.co>
2019-11-03 14:01:46 -0500

    install and configure firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireStorageModule} from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,10 @@ import {HttpClientModule} from '@angular/common/http';
     SharedModule,
     CoreModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  platziApi: 'http://platzi-store.herokuapp.com/'
+  platziApi: 'http://platzi-store.herokuapp.com/',
+  firebase: {
+    apiKey: 'AIzaSyAu8thp-K-WxpwqzFa3R3eqg0SfiGOwX1E',
+    authDomain: 'angular-store-63248.firebaseapp.com',
+    databaseURL: 'https://angular-store-63248.firebaseio.com',
+    projectId: 'angular-store-63248',
+    storageBucket: 'angular-store-63248.appspot.com',
+    messagingSenderId: '171755092768',
+    appId: '1:171755092768:web:480ee6c664b9bb440af545'
+  }
 };
